6cbb75280c8e73b532a0d4dd37e2034b19668be0
nermakov2903-lab <nermakov2903@gmail.com>
2025-11-23 18:48:48 +0700

    array + test main

---
+++ main.py
@@ -0,0 +1,21 @@
+from menu_task8 import menu_task8
+
+def main():
+    while True:
+        print("\n=== Главное меню ===")
+        print("1. Задание №8")
+        print("0. Выход")
+
+        choice = input("Выберите пункт меню: ")
+
+        if choice == "1":
+            menu_task8()
+        elif choice == "0":
+            print("Выход.")
+            break
+        else:
+            print("Ошибка: нет такого пункта меню!")
+
+if __name__ == "__main__":
+    main()
+
