4b2427f44602592dbeaba29a97f3768b2e1a355f
nermakov2903-lab <nermakov2903@gmail.com>
2025-11-23 19:35:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,11 @@ def main():
     arr = manual_input_array()
     print("Вы ввели:", arr)
 
+
+    print("Тест сортировки массива:")
+    arr = [5, 1, 4, 2, 3]
+    print("Исходный:", arr)
+    print("Отсортированный:", sort_array(arr))
+
 if __name__ == "__main__":
     main()
