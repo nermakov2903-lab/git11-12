3e2f0a9e321d6a316ecc36adbc6d9aa08f4a989f
nermakov2903-lab <nermakov2903@gmail.com>
2025-11-23 19:06:01 +0700

    menu + main

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-from src.task8_common_numbers import count_common_numbers
+from src.menu_task8 import menu_task8
 
 def main():
-    print("Тест алгоритма задания 8:")
-    arr1 = [12, 23, 34, 45]
-    arr2 = [21, 23, 43, 50]
-    print("arr1:", arr1)
-    print("arr2:", arr2)
-    result = count_common_numbers(arr1, arr2)
-    print("Количество общих чисел (с учётом переворота):", result)
+    print("Запуск меню задания 8")
+    menu_task8()
 
 if __name__ == "__main__":
     main()
