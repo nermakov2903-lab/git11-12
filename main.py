7d25c25948492cf45923f5f8ceecc4b98e0544c8
nermakov2903-lab <nermakov2903@gmail.com>
2025-11-23 19:59:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from src.menu_task8 import menu_task8
 
 def main():
-    print("Запуск меню задания 8")
-    menu_task8()
+    while True:
+        print("\n=== Главное меню ===")
+        print("1. Задание №8")
+        print("0. Выход")
+
+        choice = input("Выберите пункт меню: ")
+
+        if choice == "1":
+            menu_task8()
+        elif choice == "0":
+            print("Выход.")
+            break
+        else:
+            print("Ошибка: нет такого пункта меню!")
 
 if __name__ == "__main__":
     main()
