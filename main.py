34f3284698b513d8138e5f9dc3f214947904bccd
nermakov2903-lab <nermakov2903@gmail.com>
2025-11-24 17:27:48 +0700

    sort + main

---
+++ main.py
@@ -0,0 +1,10 @@
+from array_sort import sort_array
+
+def main():
+    print("Тест сортировки массива:")
+    arr = [5, 1, 4, 2, 3]
+    print("Исходный:", arr)
+    print("Отсортированный:", sort_array(arr))
+
+if __name__ == "__main__":
+    main()
