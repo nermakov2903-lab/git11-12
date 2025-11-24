9c6a2279a6ebccf807ef92db9e35ec665667928d
nermakov2903-lab <nermakov2903@gmail.com>
2025-11-24 17:26:41 +0700

    generate + main

---
+++ main.py
@@ -0,0 +1,9 @@
+from array_generate import generate_array
+
+def main():
+    print("Тест генерации массива:")
+    arr = generate_array(5, 0, 9)
+    print("Сгенерированный массив:", arr)
+
+if __name__ == "__main__":
+    main()
