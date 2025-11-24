59f52eeadda461664ba6a212e41c418c71ab992e
nermakov2903-lab <nermakov2903@gmail.com>
2025-11-24 17:30:38 +0700

    merge + main

---
+++ main.py
@@ -0,0 +1,12 @@
+from array_merge import merge_arrays
+
+def main():
+    print("Тест объединения массивов:")
+    arr1 = [1, 2, 3]
+    arr2 = [4, 5, 6]
+    print("arr1:", arr1)
+    print("arr2:", arr2)
+    print("Объединение:", merge_arrays(arr1, arr2))
+
+if __name__ == "__main__":
+    main()
