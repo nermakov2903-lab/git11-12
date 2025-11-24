0da9c9f2a4dd2cd6d689af1053023b65000b976a
nermakov2903-lab <nermakov2903@gmail.com>
2025-11-24 17:24:59 +0700

    input + main

---
+++ main.py
@@ -0,0 +1,9 @@
+from src.array_input import manual_input_array
+
+def main():
+    print("Тест функции ручного ввода массива:")
+    arr = manual_input_array()
+    print("Вы ввели:", arr)
+
+if __name__ == "__main__":
+    main()
